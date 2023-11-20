0a08ef6adb6dbae8be9baaa6a0fedef87bf77fbe
safdar211ali <safdar211@hotmail.com>
2023-11-20 23:24:10 +0500

    updated new

--- garments_reports/garments_reports/report/master_costing_comparison/master_costing_comparison.py
+++ garments_reports/garments_reports/report/master_costing_comparison/master_costing_comparison.py
@@ -9,6 +9,7 @@ def execute(filters=None):
     data = []
     columns = get_columns()
     data = get_data(filters)
+
     return columns, data
 
 
@@ -44,9 +45,12 @@ def get_columns():
 
 def get_conditions(filters, doctype):
     conditions = []
-
-    if filters.get("name"):
-        conditions.append(f"`{doctype}`.master_towel_costing = %(name)s")
+    if doctype == 'sr':
+        if filters.get("name"):
+            conditions.append(f"`{doctype}`.master_towel_costing = %(name)s")
+    elif doctype == 'mtc':
+        if filters.get("name"):
+            conditions.append(f"`{doctype}`.name = %(name)s")
     return " AND ".join(conditions)
 
 
@@ -68,31 +72,53 @@ def get_data(filters):
             """.format(conditions=get_conditions(filters, "sr"))
 
     srsi_result = frappe.db.sql(srsi_query, filters, as_dict=1)
-    # for row in si_result:
-    #     row.update({
-    #         'cargo_in_tons': row.import_teus + row.import_teus,
-    #     })
-    # total_import_teus = 0
-    # total_export_teus = 0
-    # total_grand_total = 0
-    # total_credit = 0
-    # total_outstanding_amount = 0
-    # total_customer_name = len(si_result)
-    # for row in si_result:
-    #     total_grand_total += row["grand_total"]
-    #     total_credit += row["credit"]
-    #     total_outstanding_amount += row["outstanding_amount"]
-    #     total_import_teus += row["import_teus"]
-    #     total_export_teus += row["export_teus"]
-    # si_result.append({
-    #     "customer": _("Total"),
-    #     "customer_name": f"Total parties :  {total_customer_name}",
-    #     "import_teus": total_import_teus,
-    #     "export_teus": total_export_teus,
-    #     "cargo_in_tons": total_import_teus + total_export_teus,
-    #     "grand_total": total_grand_total,
-    #     "credit": total_credit,
-    #     "outstanding_amount": total_outstanding_amount
-    # })
+
+    mtc_query = """
+            SELECT 
+                mtcrm.raw_material AS rm_item_code,
+                ROUND(SUM(mtcrm.yarn_required_in_lbs),2) AS consumed_qty,
+                ROUND(SUM(mtcrm.bags_reqd)) AS bags,
+                SUM(mtcrm.cost) AS amount
+            FROM 
+                `tabMaster Towel Costing` AS mtc, `tabMaster Towel Costing Raw Material` AS mtcrm, `tabItem` AS item
+            WHERE
+                mtcrm.raw_material = item.item_code AND item.item_group = 'Yarn' AND mtc.docstatus < 1 AND mtc.name = mtcrm.parent AND
+                {conditions}
+            GROUP BY
+                mtcrm.raw_material
+            """.format(conditions=get_conditions(filters, "mtc"))
+
+    mtc_result = frappe.db.sql(mtc_query, filters, as_dict=1)
+
+    # sum for first query
+    total_consumed_qty = 0
+    total_bags = 0
+    total_amount = 0
+    for row in srsi_result:
+        total_consumed_qty += row["consumed_qty"]
+        total_bags += row["bags"]
+        total_amount += row["amount"]
+    srsi_result.append({
+        "rm_item_code": _("<b>Total</b>"),
+        "consumed_qty": f"<b>{total_consumed_qty}</b>",
+        "bags": f"<b>{total_bags}</b>",
+        "amount": total_amount,
+    })
+    # sum for first query
+    total_yarn_required_in_lbs = 0
+    total_bags_reqd = 0
+    total_cost = 0
+    for row in mtc_result:
+        total_yarn_required_in_lbs += row["consumed_qty"]
+        total_bags_reqd += row["bags"]
+        total_cost += row["amount"]
+    mtc_result.append({
+        "rm_item_code": _("<b>Total</b>"),
+        "consumed_qty": f"<b>{total_yarn_required_in_lbs}</b>",
+        "bags": f"<b>{total_bags_reqd}</b>",
+        "amount": total_cost,
+    })
+
     data.extend(srsi_result)
+    data.extend(mtc_result)
     return data
